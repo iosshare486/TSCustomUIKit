06f146add08cd579b15bce8b4ca3796382da0583
renpengjie <[renpengjie@caiqr.com]>
2018-08-11 15:47:52 +0800

    #13

--- TSCustomUIKit/ViewController.swift
+++ TSCustomUIKit/ViewController.swift
@@ -13,8 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let button = UIButton.init(frame: CGRect.init(x: 50, y: 50, width: 50, height: 50))
+        button.backgroundColor = UIColor.red
+        button.addTarget(self, action: #selector(buttonClick), for: UIControlEvents.touchUpInside)
+        self.view.addSubview(button)
     }
 
+    @objc func buttonClick() -> Void {
+//        ts_toastControl.animationStyle = .upDownAndFade
+        TSToastControl.showToast("我")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
